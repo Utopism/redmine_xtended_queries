b25aee90b0c0d2be99465f47ea074120f3bc110b
Lxc <mel+lxc@ennder.fr>
2020-01-31 11:05:38 +0100

    V1.0.10 Re-introducing Query watcher_values method for Older Redmine versions
    modifié :         README.md
    modifié :         init.rb
    modifié :         lib/models/smile_models_query.rb

--- init.rb
+++ init.rb
@@ -18,7 +18,7 @@ Redmine::Plugin.register plugin_name do
   author_url "mailto:Jerome BATAILLE <redmine-support@smile.fr>?subject=#{plugin_name}"
   description 'Extends the Redmine Queries and adds small Improvments'
   url "https://github.com/Smile-SA/#{plugin_name}"
-  version '1.0.9'
+  version '1.0.10'
   requires_redmine :version_or_higher => '2.6.0'
 
   requires_redmine_plugin :redmine_smile_base, :version_or_higher => '1.0.0'
@@ -169,6 +169,10 @@ rails_dispatcher.to_prepare do
   prepend_in(Project, Smile::Models::ProjectOverride::ExtendedQueries)
 
   prepend_in(Query, Smile::Models::QueryOverride::ExtendedQueries)
+
+  unless Query.instance_methods.include?(:watcher_values)
+    prepend_in(Query, Smile::Models::QueryOverride::WatcherValues)
+  end
 
   prepend_in(IssueQuery, Smile::Models::IssueQueryOverride::ExtendedQueries)
 
--- lib/models/smile_models_query.rb
+++ lib/models/smile_models_query.rb
@@ -10,6 +10,8 @@
 # * #763000 Totalable colums : hide checkbox if not enable on project nor sub-projects
 #   DISABLED
 
+# 2/ module WatcherValues
+
 #require 'active_support/concern' #Rails 3
 
 module Smile
@@ -1409,6 +1411,68 @@ module Smile
           end
         end # module ClassMethods
       end # module ExtendedQueries
+
+      #*****************
+      # 2/ WatcherValues
+      module WatcherValues
+        # extend ActiveSupport::Concern
+
+        def self.prepended(base)
+          #####################
+          # 1/ Instance methods
+          watcher_values_instance_methods = [
+            :watcher_values,  #  1/ new method  TESTED           OK
+          ]
+
+          trace_prefix = "#{' ' * (base.name.length + 27)}  --->  "
+          last_postfix = '< (SM::MO::QueryOverride::WatcherValues)'
+
+
+          smile_instance_methods = base.public_instance_methods.select{|m|
+              base.instance_method(m).owner == self
+            }
+          smile_instance_methods += base.protected_instance_methods.select{|m|
+              base.instance_method(m).owner == self
+            }
+
+          smile_instance_methods += base.private_instance_methods.select{|m|
+              base.instance_method(m).owner == self
+            }
+
+          missing_instance_methods = watcher_values_instance_methods.select{|m|
+            !smile_instance_methods.include?(m)
+          }
+
+          if missing_instance_methods.any?
+            trace_first_prefix = "#{base.name} MISS            instance_methods  "
+          else
+            trace_first_prefix = "#{base.name}                 instance_methods  "
+          end
+
+          SmileTools::trace_by_line(
+            (
+              missing_instance_methods.any? ?
+              missing_instance_methods :
+              smile_instance_methods
+            ),
+            trace_first_prefix,
+            trace_prefix,
+            last_postfix,
+            :redmine_xtended_queries
+          )
+
+          if missing_instance_methods.any?
+            raise trace_first_prefix + missing_instance_methods.join(', ') + '  ' + last_postfix
+          end
+        end # def self.prepended
+
+        # Smile specific : if not defined upstream on previous Redmine versions
+        def watcher_values
+          watcher_values = [["<< #{l(:label_me)} >>", "me"]]
+          watcher_values += users.sort_by(&:status).collect{|s| [s.name, s.id.to_s, l("status_#{User::LABEL_BY_STATUS[s.status]}")] } if User.current.allowed_to?(:view_issue_watchers, self.project)
+          watcher_values
+        end
+      end # module WatcherValues
     end # module QueryOverride
   end # module Models
 end # module Smile
